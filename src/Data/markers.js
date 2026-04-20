cbaa49bfd5d91a9cbaedf8d770819ffb293bfae2
Gio <@Gio>
2026-04-21 02:50:07 +0800

    fixed the coordinates

--- src/Data/markers.js
+++ src/Data/markers.js
@@ -1,22 +1,43 @@
 export const markers = [
+   {
+    name: "Dimsum Treats",
+    geocode: [14.641067784397437, 121.07431948184968],
+    popUp: "This is Dimsum Treats"
+  },
+
   {
     name: "Tetsuo",
-    geocode: [14.637, 121.074],
+    geocode: [14.640781133731318, 121.07432484626771],
     popUp: "This is Tetsuo"
   },
+
   {
-    name: "BonChon",
-    geocode: [14.638, 121.074],
-    popUp: "This is BonChon"
+    name: "Samgyupsalamat",
+    geocode: [14.6368860, 121.07403],
+    popUp: "This is Samgyupsalamat"
   },
+
   {
-    name: "Cinnabon",
-    geocode: [14.6375, 121.075],
-    popUp: "This is Cinnabon"
+    name: "Busan",
+    geocode: [14.640108309418078,121.07342898845674],
+    popUp: "This is Busan"
   },
+
   {
-    name: "Kenny Rogers",
-    geocode: [14.637503209937096, 121.0741026954652],
-    popUp: "This is Kenny Rogers"
-  }
+    name: "Osaka Izakaya",
+    geocode: [14.640199309418078,121.07348898845674],
+    popUp: "This is Osaka Izakaya"
+  },
+
+  {
+    name: "Takaw",
+    geocode: [14.63986883821053,121.07340289520265],
+    popUp: "This is Takaw"
+  },
+
+  {
+    name: "Kanto Free Style",
+    geocode: [14.640667755919445,121.0740700051285],
+    popUp: "This is Kanto Free Style"
+  },
 ]
