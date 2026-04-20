6de8b1684ab4ef688273e4a41102213c0b3a780f
Gio <@Gio>
2026-04-21 02:49:48 +0800

    importing both restaurants and markers for flexibility

--- src/Components/MapPanel.jsx
+++ src/Components/MapPanel.jsx
@@ -1,5 +1,6 @@
 import Map from './map.jsx' 
 import { restaurants } from '../Data/restaurants.js'
+import { markers } from '../Data/markers.js'
 
 function MapPanel({setCurrentPage}){
     return (
