54d0a24f06318c02acf9f33835ee6acac254d129
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2026-04-20 19:22:32 +0800

    Style: ReviewCard

--- src/Components/ReviewCard.jsx
+++ src/Components/ReviewCard.jsx
@@ -1,48 +1,33 @@
-// export const restaurants = [
-//   {
-//     id: 1,
-//     name: "Wildflour Cafe",
-//     cuisine: "Bakery / American",
-//     price: "$$",
-//     image: "https://placehold.co/400x300",
-//     description: "Restaurant descriptive review placeholder. This restaurant description should be around 1-3 sentences.",
-//     ratings: {
-//       overall: 4.5,
-//       foodAndDrink: 4.7,
-//       service: 4.2,
-//       ambiance: 4.6,
-//       value: 4.0
-//     }
-//   },
-
 function ReviewCard({restaurant}){
 
     // turns restaurant data into local variables
-    const {name, cuisine, price} = restaurant;
+    const {name, cuisine, price, description} = restaurant;
     let {overall, foodAndDrink, service, ambiance, value} = restaurant.ratings;
 
 
     return (
-       <div className='ReviewCard'>
+       <div className='revCard'>
             <div className='revcard-restaurant-details'>
-                <img src="" alt="Dining emoji"/>
+                <div id="dining-emoji">🍽️</div>
                 <h1>{name}</h1>
                 <h4>{cuisine} ● Katip ● {price}</h4>                
             </div>
+            <hr></hr>
+            
             <div className='revcard-overall-rating'>
-                <h3>Overall</h3>
-                <p>★★★★★</p>
-                <h4>{overall}<span>/5</span></h4>
+                <div>
+                    <h3>Overall</h3>
+                    <p className="star">★★★★★</p>
+                </div>
+                <h4>{overall}<span> / 5</span></h4>
             </div>
 
             <div className='revcard-description'>{description}</div>
-            <h3>breakdown</h3>
-            <div className='revcard-criterion'>
-                <div><h6>Food & Drink</h6><p>★★★★★</p></div>
-                <div><h6>Service</h6><p>★★★★★</p></div>
-                <div><h6>Ambiance</h6><p>★★★★★</p></div>
-                <div><h6>Value</h6><p>★★★★★</p></div>
-            </div>
+            <h3 id="breakdown">breakdown</h3>
+            <div className="criterion"><p>Food & Drink</p><p className="star">★★★★★</p></div>
+            <div className="criterion"><p>Service</p><p className="star">★★★★★</p></div>
+            <div className="criterion"><p>Ambiance</p><p className="star">★★★★★</p></div>
+            <div className="criterion"><p>Value</p><p className="star">★★★★★</p></div>
        </div>
     )
 }
