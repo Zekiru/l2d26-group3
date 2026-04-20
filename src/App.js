d595d4a402bc1b598a71e9c89968344ad092a99d
KaitoMikazuki <175144170+KaitoMikazuki@users.noreply.github.com>
2026-04-21 00:15:12 +0800

    Feature: ReviewPage feature where user can view all restaurants together

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Map from './Components/map.jsx'
 import React from 'react';
 import MeetTheTeam from './Components/MeetTheTeam';
 import { markers } from './Data/markers.js'
+import ReviewPage from './Components/ReviewPage.jsx';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('home')
@@ -12,7 +13,7 @@ function App() {
     <>
     {currentPage === 'home' && <Homepage setCurrentPage={setCurrentPage} />}
     {currentPage === 'map' && <Map markers={markers} setCurrentPage={setCurrentPage} />}
-    {currentPage === 'resto' && <Homepage setCurrentPage={setCurrentPage} />}
+    {currentPage === 'resto' && <ReviewPage setCurrentPage={setCurrentPage} />}
     {currentPage === 'team' && <MeetTheTeam setCurrentPage={setCurrentPage} />}
     </>
   )
