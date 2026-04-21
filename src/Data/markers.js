c3f32362179bb1c322d4c33feb80e9fb6aeef88b
Zeke_L5 <zekiel.villasurda@gmail.com>
2026-04-21 15:11:16 +0800

    Update Resto Data & Markers

--- src/Data/markers.js
+++ src/Data/markers.js
@@ -1,43 +1,47 @@
 export const markers = [
-   {
+  {
     name: "Dimsum Treats",
-    geocode: [14.641067784397437, 121.07431948184968],
+    geocode: [14.63955285217509, 121.07431589841603],
     popUp: "This is Dimsum Treats"
   },
-
   {
     name: "Tetsuo",
-    geocode: [14.640781133731318, 121.07432484626771],
+    geocode: [14.64092945371918, 121.07426651005689],
     popUp: "This is Tetsuo"
   },
-
-  {
-    name: "Samgyupsalamat",
-    geocode: [14.6368860, 121.07403],
-    popUp: "This is Samgyupsalamat"
-  },
-
   {
     name: "Busan",
-    geocode: [14.640108309418078,121.07342898845674],
+    geocode: [14.646815254487176, 121.07270891202981],
     popUp: "This is Busan"
   },
-
   {
     name: "Osaka Izakaya",
-    geocode: [14.640199309418078,121.07348898845674],
+    geocode: [14.640332995686517, 121.07346131190823],
     popUp: "This is Osaka Izakaya"
   },
-
   {
-    name: "Takaw",
-    geocode: [14.63986883821053,121.07340289520265],
-    popUp: "This is Takaw"
+    name: "Takaw Taqueria",
+    geocode: [14.639902071463831, 121.07334308122103],
+    popUp: "This is Takaw Taqueria"
   },
-
   {
-    name: "Kanto Free Style",
-    geocode: [14.640667755919445,121.0740700051285],
-    popUp: "This is Kanto Free Style"
+    name: "Samgyupsalamat",
+    geocode: [14.63710796004055, 121.07400335238516],
+    popUp: "This is Samgyupsalamat"
   },
-]
+  {
+    name: "Brickfire",
+    geocode: [14.63653242539244, 121.07293599656442],
+    popUp: "This is Brickfire"
+  },
+  {
+    name: "Eat Fresh Katipunan (Southbound)",
+    geocode: [14.619284949651913, 121.07159441770403],
+    popUp: "This is Eat Fresh (Southbound)"
+  },
+  {
+    name: "Mendokoro Ramenba Katipunan",
+    geocode: [14.620988421065118, 121.0735521830722],
+    popUp: "This is Mendokoro Ramenba"
+  }
+];
